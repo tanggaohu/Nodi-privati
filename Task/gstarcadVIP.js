1583670d0bff4f7277ddb3acf495cd170087f9a9
tanggaohu <59922073+tanggaohu@users.noreply.github.com>
2022-10-29 23:51:12 +0800

    Update gstarcadVIP.js

--- Task/gstarcadVIP.js
+++ Task/gstarcadVIP.js
@@ -18,76 +18,83 @@ hostname=user.gstarcad.com
 
 
 var body = {
-  "code": "0",
-  "msg": "",
-  "rs": {
-    "active": {
-      "app_try_get_show": false,
-      "app_try_give_id": 9,
-      "app_try_give_show": false,
-      "app_try_give_usable": false
+  "code" : "0",
+  "rs" : {
+    "serverLink" : "https:\/\/cloud.gstarcad.com\/getServerLink.do",
+    "expiredList" : [
+
+    ],
+    "serverUrl" : "https:\/\/user.gstarcad.com\/login",
+    "utoken" : "7d9229cc-2011-416b-9195-eb863e75d57d",
+    "vipInfo" : {
+      "serverEndTimeStamp" : 4102415999,
+      "amount" : 10,
+      "numbers" : 10000,
+      "storages" : 5120,
+      "appTryStatus" : false,
+      "myAmountNumbers" : "10个群组，每个群组限10,000人",
+      "serverEndTime" : "2022-12-31 23:59:59",
+      "goodId" : 125,
+      "clientAlias" : "Web_PC,Web_H5,Web_Weixin,DF_Android,DF_iOS,DF_Windows,MPWEIXIN",
+      "canBuy" : true,
+      "canUpgrade" : false,
+      "goodsCodes" : "ML2",
+      "descs" : "高级账户",
+      "serverStartTime" : "2022-10-29 00:00:00",
+      "serverStartTimeStamp" : 1666972800000,
+      "name" : "高级账户"
     },
-    "advertisingUrl": "http://web.gstarcad.com/upgrade?uid=22636977&utoken=36fd0db1-63c2-4b56-a847-3b1f8fc9c65a",
-    "chat": {
-      "ordinary": {
-        "createGroupNumberLimit": 3,
-        "createGroupUserNumberLimit": 200,
-        "joinGroupNumberLimit": 500
+    "active" : {
+      "app_try_give_usable" : false,
+      "app_try_get_show" : false,
+      "app_try_give_show" : false,
+      "app_try_give_id" : 9
+    },
+    "recentlyExpired" : [
+
+    ],
+    "advertisingUrl" : "http:\/\/web.gstarcad.com\/upgrade?uid=22636977&utoken=7d9229cc-2011-416b-9195-eb863e75d57d",
+    "serverCode" : "CN",
+    "storage" : {
+      "free" : 0,
+      "surplus" : 5120,
+      "total" : 5120,
+      "used" : 0
+    },
+    "vipList" : [
+
+    ],
+    "userInfo" : {
+      "id" : 22636977,
+      "updateTime" : "",
+      "trialType" : 0,
+      "registerDate" : "2022-10-13 23:42:12",
+      "mobile" : "13022891706",
+      "sex" : "",
+      "level" : 1,
+      "nickName" : "Skinny Tiger",
+      "lastLoginTime" : "2022-10-13 23:42:12",
+      "vipType" : 2,
+      "isActivate" : 1,
+      "name" : "Skinny Tiger",
+      "score" : 22
+    },
+    "chat" : {
+      "userSig" : "eJw1jkELgjAYhv-LzqHfpu1zQgelDrGow7rUTXLGpxnLiQTRf09aHd-n5YHnxY47E9mno8GyXEKaASy*bLIDy5mIgIXt665yjmqW8xQgQVSKh4dqex*poSAImUiF*LfoOsPN5WHaW*HKuI1PhZ5aj2t92JfERXdu9LZqwBut*qzwq584Uj8HcSkRlihU9v4AExIwXg__",
+      "ordinary" : {
+        "joinGroupNumberLimit" : 500,
+        "createGroupNumberLimit" : 3,
+        "createGroupUserNumberLimit" : 200
       },
-      "senior": {
-        "createGroupNumberLimit": 20,
-        "createGroupUserNumberLimit": 200,
-        "joinGroupNumberLimit": 500
-      },
-      "userSig": "eJyrVgrxCdZLrSjILEpVsjIzMLEwMNABi5WlFilZKRnpGShB*MUp2YkFBZkpSlaGJgYGxubmlpaGEJnMlNS8ksy0TIgGIzNjM0tzc5iuzHSgYLJhZGieV154WZC-Sa5FXmFEkkdBmEWFY4aJtpdZUWiuUWlgeJZpWWKkhastVGNJZi7QQYZmZuYGhqYmFma1ACSpMLU_"
-    },
-    "expiredList": [],
-    "recentlyExpired": [],
-    "serverCode": "CN",
-    "serverLink": "https://cloud.gstarcad.com/getServerLink.do",
-    "serverUrl": "https://user.gstarcad.com/login",
-    "storage": {
-      "free": 0,
-      "surplus": 5120,
-      "total": 5120,
-      "used": 0
-    },
-    "userInfo": {
-      "id": 22636977,
-      "isActivate": 1,
-      "lastLoginTime": "2022-10-13 23:42:12",
-      "level": 1,
-      "mobile": "13022891706",
-      "name": "Skinny Tiger",
-      "nickName": "Skinny Tiger",
-      "registerDate": "2022-10-13 23:42:12",
-      "score": 22,
-      "sex": "",
-      "trialType": 0,
-      "updateTime": "",
-      "vipType": 2
-    },
-    "utoken": "36fd0db1-63c2-4b56-a847-3b1f8fc9c65a",
-    "vipInfo": {
-      "amount": 10,
-      "appTryStatus": false,
-      "canBuy": true,
-      "canUpgrade": false,
-      "clientAlias": "Web_PC,Web_H5,Web_Weixin,DF_Android,DF_iOS,DF_Windows,MPWEIXIN",
-      "descs": "高级账户",
-      "goodId": 125,
-      "goodsCodes": "ML2",
-      "myAmountNumbers": "10个群组，每个群组限10,000人",
-      "name": "高级账户",
-      "numbers": 10000,
-      "serverEndTime": "2099-12-31 23:59:59",
-      "serverEndTimeStamp": 4102415999000,
-      "serverStartTime": "2022-10-29 00:00:00",
-      "serverStartTimeStamp": 1666972800000,
-      "storages": 5120
-    },
-    "vipList": []
+      "senior" : {
+        "joinGroupNumberLimit" : 500,
+        "createGroupNumberLimit" : 20,
+        "createGroupUserNumberLimit" : 200
+      }
+    }
   },
-  "status": true
-  }
-$done({ body });
+  "status" : true,
+  "msg" : ""
+}
+};
+$done({body: JSON.stringify(body)});
