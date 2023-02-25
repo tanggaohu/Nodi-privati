b91a801fa4d8a87e33938739ed1e40ea44144c96
tanggaohu <59922073+tanggaohu@users.noreply.github.com>
2023-02-25 21:58:13 +0800

    Update XTJY.js

--- Task/XTJY.js
+++ Task/XTJY.js
@@ -19,10 +19,10 @@ TG频道群：https://t.me/WeiRenQAQ
 
 #醒图+剪映会员+轻颜相机会员
 
-^http[s]?:\/\/.+((retouchpics)|(faceu)|(ulikecam)).(com|cn|mobi)\/.+(user|\/v1\/homepage\/pro|ever_photo\/space_info).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XTJY.js
-^http[s]?:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info\?(iid|.&aid|.&iid).*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/xintu.js
-^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/JY.js
-^http[s]?:\/\/commerce-api.faceu.mobi\/commerce\/v1\/subscription\/user_info.*total_memory.*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/jxyq.js
+^http[s]?:\/\/.+((retouchpics)|(faceu)|(ulikecam)).(com|cn|mobi)\/.+(user|\/v1\/homepage\/pro|ever_photo\/space_info).*$ url script-response-body https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/XTJY.js
+^http[s]?:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info\?(iid|.&aid|.&iid).*$ url script-request-header https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/xintu.js
+^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/JY.js
+^http[s]?:\/\/commerce-api.faceu.mobi\/commerce\/v1\/subscription\/user_info.*total_memory.*$ url script-request-header https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/jxyq.js
 
 [mitm]
 hostname = *faceu*,*retouchpics*,*.ulikecam.com
