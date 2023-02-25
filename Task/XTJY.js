de878379b37fa512b917a0ec055e9db1c675a425
tanggaohu <59922073+tanggaohu@users.noreply.github.com>
2023-02-25 21:01:38 +0800

    Update XTJY.js

--- Task/XTJY.js
+++ Task/XTJY.js
@@ -13,5 +13,5 @@ hostname = *faceu*,*retouchpics*,*.ulikecam.com
 ***********************************/
 
 
-body = $response.body.replace(/"is_vip":\w+/g, '"is_vip":true');
+body = $response.body.replace(/"is_vip":\w+/g, '"is_vip":true').replace(/"end_time":\d+/g, '"end_time":4102415999');
 $done({ body });
