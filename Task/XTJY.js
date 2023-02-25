09bc4ea715875b4de8171182b5fccc9370babcc9
tanggaohu <59922073+tanggaohu@users.noreply.github.com>
2023-02-25 20:13:47 +0800

    Update XTJY.js

--- Task/XTJY.js
+++ Task/XTJY.js
@@ -2,7 +2,7 @@
  
 [rewrite_local]
 #醒图+剪映会员+轻颜相机会
-# ～ moneypro解锁会员权限（2023-02-09）@Skinny Tiger
+# ～ 醒图剪映解锁会员权限（2023-02-09）@Skinny Tiger
 ^http[s]?:\/\/.+((retouchpics)|(faceu)|(ulikecam)).(com|cn|mobi)\/.+(user|\/v1\/homepage\/pro|ever_photo\/space_info).*$ url script-response-body https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/XTJY.js
 ^http[s]?:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info\?(iid|.&aid|.&iid).*$ url script-request-header https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/xintu.js
 ^http[s]?:\/\/commerce-api-.*\.faceu\.mobi\/commerce\/v1\/.*$ url script-request-header https://raw.githubusercontent.com/tanggaohu/Nodi-privati/main/Task/JY.js
